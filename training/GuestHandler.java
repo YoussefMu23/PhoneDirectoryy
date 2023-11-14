e3e00c3a567aad6c2dccdc5f7e3dd84a146a5d5b
YoussefMu23 <148534137+YoussefMu23@users.noreply.github.com>
2023-11-14 11:17:36 +0100

    Update GuestHandler.java

--- training/GuestHandler.java
+++ training/GuestHandler.java
@@ -7,48 +7,57 @@ public class GuestHandler implements UserActions {
         this.contactList = contactList;
     }
 
+    // This function allow us to search for last name in contactList. 
     @Override
     public void searchByLastName(String lastName) {
-
+        
         for (Contact contact : contactList.getContacts()) {
-            if (contact.getLastName().equalsIgnoreCase(lastName)) {
+            
+            if (contact.getLastName().equals(lastName)) {
+                
                 System.out.println(contact);
             }
         }
     }
 
+    // This function allow us to search for first name in contactList.
     @Override
     public void searchByFirstName(String firstName) {
 
         for (Contact contact : contactList.getContacts()) {
-            if (contact.getFirstName().equalsIgnoreCase(firstName)) {
+            
+            if (contact.getFirstName().equals(firstName)) {
+                
                 System.out.println(contact);
             }
         }
     }
 
+    // This function allow us to search for address in contactList.
     @Override
     public void searchByAddress(String address) {
 
         for (Contact contact : contactList.getContacts()) {
-            if (contact.getAddress().equalsIgnoreCase(address)) {
+            
+            if (contact.getAddress().equals(address)) {
+                
                 System.out.println(contact);
             }
         }
     }
 
-    
-    // // This function allow us to freestyle Search 
+    // // This function allow us to search for in contactList.
     @Override
     public void freestyleSearch(String searchFree) {
 
         for (Contact contact : contactList.getContacts()) {
+            
             if (contact.getLastName().contains(searchFree) || contact.getFirstName().contains(searchFree) ||
                     contact.getAddress().contains(searchFree) || contact.getPhoneNumber().contains(searchFree)) {
+                
                 System.out.println(contact);
             }
         }
     }
-
 
 }
