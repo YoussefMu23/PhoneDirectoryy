a222bc41ed42a8135a23f3f158c823827b9c3dab
YoussefMu23 <148534137+YoussefMu23@users.noreply.github.com>
2023-11-14 11:19:54 +0100

    Update UserActions.java

--- training/UserActions.java
+++ training/UserActions.java
@@ -4,8 +4,12 @@ package training;
  * https://www.w3schools.com/java/java_interface.asp
  */
 public interface UserActions {
+    
     void searchByLastName(String lastName);
+   
     void searchByFirstName(String firstName);
+    
     void searchByAddress(String address);
+    
     void freestyleSearch(String searchTerm);
 }
